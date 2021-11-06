aed569037b1a18b0442056ab531a7beaeb74378a
Matt DeZarn <matt.dezarn@gmail.com>
2021-11-06 16:59:55 -0700

    made db folder with inserted users

---
+++ db/create_nerds.sql
@@ -0,0 +1,7 @@
+INSERT INTO main_app_nerds(gm, name, img, bio, created_at)
+  VALUES
+    ("t", "Dingus", "https://i.imgur.com/B04AVJZ.jpeg", "I am a DM. Bow to me!", "2021-05-07 18:57:42.517205-07");
+
+INSERT INTO main_app_nerds(gm, name, img, bio, created_at)
+  VALUES
+    ("f", "Dongus", "https://i.imgur.com/T0WlDmD.jpeg", "I am a lowly player with doodoo for brains.", "Q2021-05-07 18:57:42.517205-07");
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Nerd(models.Model):
+    gm = models.BooleanField(default=False)
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    bio = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
